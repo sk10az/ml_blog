daa70e428ea1605e29d55054941ae43b459517a7
Danil <sk10azik@gmail.com>
2023-03-26 22:37:34 +0300

    добавил переход на страницу поста

--- ml_blog/posts/urls.py
+++ ml_blog/posts/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.posts, name='posts'),
+
+    # path('login/', views.loginUser, name="login"),
+    # path('logout/', views.logoutUser, name="logout"),
+    # path('register/', views.registerUser, name="register"),
+
+    path('post/<str:pk>/', views.viewPost, name='post'),
+    # path('add/', views.addPhoto, name='add'),
 ]
